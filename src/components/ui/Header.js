2868f196598679495d5c6c26ec6e4892d6ad5733
Jhonatan Soto <jhonrocker2012@gmail.com>
2021-10-20 23:35:19 -0400

    add menu in header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,49 +1,88 @@
-import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
-import React from 'react';
+import {
+  AppBar,
+  Button,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { findByLabelText } from "@testing-library/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const headersData = [
   {
-    label: 'Listings',
-    href: '/listings',
+    label: "Collections",
+    href: "/collections",
   },
   {
-    label: 'Mentors',
-    href: '/mentors',
+    label: "Men",
+    href: "/men",
   },
   {
-    label: 'My Account',
-    href: '/account',
-  },
-  {
-    label: 'Log Out',
-    href: '/logout',
+    label: "Women",
+    href: "/women",
   },
 ];
 
 const useStyles = makeStyles(() => ({
   header: {
-    backgroundColor: '#400CCC',
+    backgroundColor: "#400CCC",
+    paddingRight: "79px",
+    paddingLeft: "118px",
   },
   logo: {
-    fontFamily: 'Kumbh Sans',
+    fontFamily: "Kumbh Sans",
     fontWeight: 700,
-    color: '#FFFEFE',
-    textAlign: 'left',
+    color: "#FFFEFE",
+    textAlign: "left",
+  },
+  menuButton: {
+    fontFamily: "Kumbh Sans, sans-serif",
+    fontWeight: 400,
+    size: "18px",
+    marginLeft: "38px",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 }));
 
 export const Header = () => {
-  const { header, logo } = useStyles();
+  const { header, logo, menuButton, toolbar } = useStyles();
 
   const displayDesktop = () => {
-    return <Toolbar>{femmecubatorLogo}</Toolbar>;
+    return (
+      <Toolbar className={toolbar}>
+        {sneakersLogo}
+        <div>{getMenuButtons()}</div>
+      </Toolbar>
+    );
   };
 
-  const femmecubatorLogo = (
-    <Typography variant='h6' component='h1' className={logo}>
+  const sneakersLogo = (
+    <Typography variant="h6" component="h1" className={logo}>
       sneakers
     </Typography>
   );
+
+  const getMenuButtons = () => {
+    return headersData.map(({ label, href }) => {
+      return (
+        <Button
+          {...{
+            key: label,
+            color: "inherit",
+            to: href,
+            component: Link,
+            className: menuButton,
+          }}
+        >
+          {label}
+        </Button>
+      );
+    });
+  };
 
   return (
     <header>
